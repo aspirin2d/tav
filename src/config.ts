fb5c8428ce275af726ad388d8351fb422e1295bc
aspirin2d <aspirin2ds@gmail.com>
2025-09-17 17:34:28 +0800

    add tav

--- src/config.ts
+++ src/config.ts
@@ -2,16 +2,7 @@ import { readFileSync } from "node:fs";
 import { parse as parseToml } from "smol-toml";
 import { z } from "zod";
 
-import type { AbilityScores } from "./db/schema.js";
-
-const abilityScoresSchema = z.object({
-  str: z.number().int().min(0).max(30),
-  dex: z.number().int().min(0).max(30),
-  con: z.number().int().min(0).max(30),
-  int: z.number().int().min(0).max(30),
-  wis: z.number().int().min(0).max(30),
-  cha: z.number().int().min(0).max(30),
-});
+import { abilityScoresSchema, type AbilityScores } from "./db/schema.js";
 
 const configSchema = z.object({
   defaults: z.object({
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,13 +1,18 @@
 import { jsonb, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import z from "zod";
 
-export type AbilityScores = {
-  str: number;
-  dex: number;
-  con: number;
-  int: number;
-  wis: number;
-  cha: number;
-};
+export const abilityScoresSchema = z.object({
+  str: z.number().int().min(0).max(30),
+  dex: z.number().int().min(0).max(30),
+  con: z.number().int().min(0).max(30),
+  int: z.number().int().min(0).max(30),
+  wis: z.number().int().min(0).max(30),
+  cha: z.number().int().min(0).max(30),
+});
+
+export const skillSchema = z.object({});
+
+export type AbilityScores = z.infer<typeof abilityScoresSchema>;
 
 export const visitLog = pgTable("visit_log", {
   id: serial().primaryKey(),
